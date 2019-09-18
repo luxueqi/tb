f3d750b9bdee47dfbab4c2f089b3312076f3ebf2
jian chu <705178580@qq.com>
2019-09-19 06:20:14 +0800

    .net4

---
+++ imgweb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tieba
+{
+    public partial class imgweb : Form
+    {
+        public imgweb()
+        {
+            InitializeComponent();
+        }
+        public string html = "";
+        private void imgweb_Load(object sender, EventArgs e)
+        {
+            webBrowser1.DocumentText = html;
+        }
+    }
+}
